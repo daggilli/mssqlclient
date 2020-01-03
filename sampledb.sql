5cf8d22a786bfd0e6dd5a0b0db516b623da77753
daggilli <daggillies@gmail.com>
2020-01-03 17:46:43 -0600

    fix: tabs [develop]

--- sampledb.sql
+++ sampledb.sql
@@ -1,14 +1,14 @@
 CREATE TABLE TestDB.dbo.Events (
-	Id int IDENTITY(1,1) NOT NULL,
-	Name varchar(128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	EventTime datetime NOT NULL,
-	CONSTRAINT PK_Events PRIMARY KEY (Id)
+  Id int IDENTITY(1,1) NOT NULL,
+  Name varchar(128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+  EventTime datetime NOT NULL,
+  CONSTRAINT PK_Events PRIMARY KEY (Id)
 );
 
 CREATE TABLE TestDB.dbo.Test (
-	Id int IDENTITY(1,1) NOT NULL,
-	Value varchar(128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	CONSTRAINT PK_Test PRIMARY KEY (Id)
+  Id int IDENTITY(1,1) NOT NULL,
+  Value varchar(128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+  CONSTRAINT PK_Test PRIMARY KEY (Id)
 );
 
 CREATE PROCEDURE dbo.TestProcedure
